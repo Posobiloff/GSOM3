9ee748a80f5f174292dc66cc051701067ee6fe64
Feodosy <st086913@student.spbu.ru>
2024-10-16 09:37:13 +0000

    Add folder Assignment13

--- Assignment13/Cars_crashes analysis.py
+++ Assignment13/Cars_crashes analysis.py
@@ -4,7 +4,7 @@ cars
 
 import matplotlib.pyplot as plt
 plt.figure(figsize = (20, 10))
-sns.barplot(data=cars, x='total', y='abbrev', orient='h', width = 2)
+sns.barplot(data=cars, x='total', y='abbrev', orient='h', width = 0.5)
 
 plt.figure(figsize = (20, 10))
 sns.scatterplot(data=cars, x='speeding', y='alcohol')
@@ -19,7 +19,7 @@ plt.figure(figsize = (20, 10))
 sns.relplot(data=cars, x='ins_premium', y='ins_losses', hue = "not_distracted")
 
 plt.figure(figsize = (20, 10))
-sns.barplot(data=cars, x='ins_losses', y='abbrev', orient='h', width = 2)
+sns.barplot(data=cars, x='ins_losses', y='abbrev', orient='h', width = 0.5)
 
 plt.figure(figsize = (20, 10))
 sns.relplot(data=cars, x='not_distracted', y='no_previous', hue = "ins_premium")
